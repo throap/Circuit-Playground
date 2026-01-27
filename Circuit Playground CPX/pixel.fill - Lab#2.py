31ab9f93d57b9a2eff176fa073e44b6f7b3898e7
throap <silverlightstar99@gmail.com>
2026-01-27 13:12:01 -0800

    pixel.fill - Lab#2.py

--- Circuit Playground CPX/pixel.fill - Lab#2.py
+++ Circuit Playground CPX/pixel.fill - Lab#2.py
@@ -6,11 +6,11 @@ import neopixel
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
 while True:
-    pixels[2]=(255, 0, 0) #flash red for every other rgb light
-    pixels[0]=(255, 0, 0)
-    pixels[4]=(255, 0, 0)
-    pixels[6]=(255, 0, 0)
-    pixels[8]=(255, 0, 0)
+    pixels[2]=(0, 255, 0) #flash green for every other rgb light
+    pixels[0]=(0, 255, 0)
+    pixels[4]=(0, 255, 0)
+    pixels[6]=(0, 255, 0)
+    pixels[8]=(0, 255, 0)
     time.sleep(0.5)
     pixels.fill((225, 100, 100)) #make the default color pink-ish
     time.sleep(0.5)
