9230ef61ed2e2a714d76326564516168f19a433b
throap <silverlightstar99@gmail.com>
2026-01-29 13:12:45 -0800

    fixing errors.py

---
+++ Circuit Playground CPX/fixing errors.py
@@ -0,0 +1,16 @@
+print ("hello")
+
+x=5
+if x==5:
+    print("x is 5")
+    
+def calculate_sum(a,b):
+    total = a+b
+    return total
+x=5
+y=10
+z=calculate_sum(x,y)
+print(z)
+
+my_list = [100, 200, 300, 400]
+print(my_list[3])
