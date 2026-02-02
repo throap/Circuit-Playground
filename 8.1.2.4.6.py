d63aebcff3e6f17eb45018d25b70bda9e8a1705d
throap <silverlightstar99@gmail.com>
2026-02-02 14:42:03 -0800

    8.1.2.4.6.py

---
+++ 8.1.2.4.6.py
@@ -0,0 +1,9 @@
+def main ():
+    x = 0
+    print ('Starting while loop - counting to 10')
+    while (x < 11):
+        print (x)
+        x += 1
+    print ('End the counting program')
+
+main ()
