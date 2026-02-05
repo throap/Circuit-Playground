9cee98ab01a8a93f4a821e6bdfe498f9b4441279
throap <silverlightstar99@gmail.com>
2026-02-05 14:00:38 -0800

    8.1.3.1.10.py

--- 8.1.3.1.10.py
+++ 8.1.3.1.10.py
@@ -9,3 +9,15 @@ def challenge_3(x,y):
     return z
   else:
     return z
+
+def main():
+  print('Enter a number!')
+  number_1 = int(input())
+  print('Now eneter another number!')
+  number_2 = int(input())
+  first_challenge = challenge_2(number_1,number_2)
+  print('The mutiplication of your two numbers is', first_challenge )
+  second_challenge = challenge_3(number_1,number_2)
+  print('the sum of these numbers is', second_challenge, '(if it was odd, then it was rounded up to the nearest even)')
+
+main()
