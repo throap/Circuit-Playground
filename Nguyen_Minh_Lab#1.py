3cb69852500fd782838f2c8073bdbce41d87c029
throap <silverlightstar99@gmail.com>
2026-02-09 15:05:51 -0800

    Nguyen_Minh_Lab#1.py

---
+++ Nguyen_Minh_Lab#1.py
@@ -0,0 +1,11 @@
+x = [1, 2, 3, 4, 5, 6, 7]
+print (x)
+print (x[-1])
+
+y = ['andy', 'minh', 'dang', 'ethan']
+print (y)
+print (y[0])
+
+z = ['andy', 18, 'cant touch', 'kids', 'below', 17.5, 'years old']
+for i in range (len(z)-1):
+    print (z[i])
