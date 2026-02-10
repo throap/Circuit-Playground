c40ef85659dd222e2ea78b8ad5e09a4d79178615
throap <silverlightstar99@gmail.com>
2026-02-10 14:45:39 -0800

    Nguyen_Minh_8.1.4.1.2.py

---
+++ Nguyen_Minh_8.1.4.1.2.py
@@ -0,0 +1,19 @@
+sybau = ['b', 'e', ' ', 'q', 'u', 'i', 'e', 't']
+print (sybau[0:1])
+print (sybau[3:-1])
+print (sybau)
+
+sybau.append('!')
+print (sybau)
+
+new = sybau + ['andy']
+print (new)
+
+sybau.sort()
+print (sybau)
+
+sybau.pop()
+print (sybau)
+
+del sybau[0]
+print (sybau)
